f943bcd9e6958453cd64828834ad9c8b5f030d7a
fgimenez <fgimenez@hispasat.es>
2016-01-28 16:40:51 +0100

    it works for 25 numbers (hardcoded)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+const MAX: usize = 5000;
+
 fn main() {
-    println!("Hello, world!");
+    //First generate the first 25 ludic numbers
+    
+    print!("1,");
+    let mut array: [i32; MAX] = [0; MAX];
+
+    //Construct the first array
+    let mut num_inicial:i32 = 2;
+    for i in 0..array.len()
+    {
+    	array[i] = num_inicial;
+    	num_inicial += 1;
+    }
+    //print_array(&array);
+
+    /*for i in 0..array.len()
+    {
+    	print!(",{}",array[i]);
+    }
+    */
+
+    
+    for _ in 0..25
+    {
+    	let next_ludic = array[0];
+    	print!("{},", next_ludic);
+    	let mut contador = 0;    	
+	    for i in 0..array.len()
+	    {
+	    	//modulo operation:
+	    	//((a % b) + b) % b
+	    	let modulo = ((i as i32 % next_ludic) + next_ludic) % next_ludic;
+	    	//print!("{},",modulo);
+	    	if modulo != 0
+	    	{
+	    		//let temp_val = ; 
+	    		array[contador] = array[i];
+	    		//print!("{},", temp_val);
+	    		contador += 1;
+	    	}
+	    }
+	    //println!("");
+	    //print_array(&array);
+
+	}
+
+	
+}
+
+fn print_array(x: &[i32; MAX])
+{
+	for i in 0..x.len()
+	{
+		print!("{},", x[i]);
+	}
+	println!("");
 }
